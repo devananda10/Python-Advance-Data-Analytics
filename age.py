240d232243a76754c1ef0db8dcb81bb762e0aa9b
devananda10 <146755507+devananda10@users.noreply.github.com>
2026-02-10 10:05:36 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,5 @@
+name=input("Enter name:")
+age=int(input("Enter age:"))
+print(name,age)
+d={name:age}
+print(d)
---
+++ name.py
@@ -0,0 +1,6 @@
+def print_name(name="Devu",greeting="hello"):
+    print(f"{greeting},{name}!")
+
+print_name(name="Devu")    
+x=input("Enter name:")
+print_name(name=x)
