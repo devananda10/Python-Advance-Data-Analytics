eb47716bfadd5ec1874d0f1827d3398e472b66f7
devananda10 <146755507+devananda10@users.noreply.github.com>
2026-02-11 09:36:58 +0530

    Add files via upload

---
+++ management.py
@@ -0,0 +1,41 @@
+class Management:
+
+    def CSE(self):
+        print("This is CSE department")
+
+    def CT(self):
+        print("This is CT department")
+
+    def CO(self):
+        print("This is CO department")
+
+    def ECE(self):
+        print("This is ECE department")
+
+    def MECH(self):
+        print("This is MECH department")
+
+    def CIVIL(self):
+        print("This is CIVIL department")
+
+    def AUTOMOBILE(self):
+        print("This is AUTOMOBILE department")
+
+    def VLSI(self):
+        print("This is VLSI department")
+
+    def EEE(self):
+        print("This is EEE department")
+
+
+
+obj = Management()
+obj.CSE()
+obj.CT()
+obj.CO()
+obj.ECE()
+obj.MECH()
+obj.CIVIL()
+obj.AUTOMOBILE()
+obj.VLSI()
+obj.EEE()
