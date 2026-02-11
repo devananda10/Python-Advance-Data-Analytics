08c25578b813b2680338d0297314857d6ed4357a
devananda10 <146755507+devananda10@users.noreply.github.com>
2026-02-11 09:31:59 +0530

    Add files via upload

---
+++ Classname.py
@@ -0,0 +1,24 @@
+class PythonDataAnalytics:
+    
+    def name1(self):
+        print("Devu")
+        
+    def name2(self):
+        print("Anu")
+        
+    def name3(self):
+        print("Manu")
+        
+    def name4(self):
+        print("Neha")
+        
+    def name5(self):
+        print("Rahul")
+
+obj = PythonDataAnalytics()
+
+obj.name1()
+obj.name2()
+obj.name3()
+obj.name4()
+obj.name5()
