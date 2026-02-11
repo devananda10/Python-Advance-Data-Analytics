b6c196ea6dda861978897e73604dfad98870f4d6
devananda10 <146755507+devananda10@users.noreply.github.com>
2026-02-11 11:27:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from Classname import PythonDataAnalytics
+from management import Management
+
+friend_obj = PythonDataAnalytics()
+dept_obj = Management()
+
+friend_obj.friend1()
+friend_obj.friend2()
+friend_obj.friend3()
+friend_obj.friend4()
+friend_obj.friend5()
+
+dept_obj.CSE()
+dept_obj.CT()
+dept_obj.CO()
+dept_obj.ECE()
+dept_obj.MECH()
+dept_obj.CIVIL()
+dept_obj.AUTOMOBILE()
+dept_obj.VLSI()
+dept_obj.EEE()
